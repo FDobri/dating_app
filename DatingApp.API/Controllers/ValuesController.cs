7cb6374bad0c9ce689a3c8900ed6ff1cc1c7b8c2
DESKTOP-KE05CFV\User <filip_dobrinic@bitbucket.org>
2020-04-16 00:17:15 +0200

    Client side login and registration

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
